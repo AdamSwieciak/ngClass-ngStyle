5e769f8c91bab5711bc880e3048944415cfd949f
AdamSwieciak <adamswiec@gmail.com>
2020-10-18 21:47:31 +0200

    end this section course

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  BtnClick= false;
+  log=[]
+
+  onClickBtn() {
+    this.BtnClick = !this.BtnClick
+    // this.log.push(this.log.length+1)
+    this.log.push(new Date())
+  }
+}
